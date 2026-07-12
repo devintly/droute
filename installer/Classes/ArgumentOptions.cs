bcf2bdc5e4ca83359953591d10fa55a4d6a828f9
snowluwu <the.snowluwu@gmail.com>
2026-07-12 20:38:45 +0600

    feat(installer): ArgumentOptions.cs

---
+++ installer/Classes/ArgumentOptions.cs
@@ -0,0 +1,28 @@
+﻿using CommandLine;
+
+namespace Droute.Installer.Classes
+{
+    internal class ArgumentOptions
+    {
+        [Option('i', "install", Required = false, HelpText = "Install Droute patch.")]
+        public bool Install { get; set; }
+
+        [Option('u', "uninstall", Required = false, HelpText = "Remove Droute patch.")]
+        public bool Uninstall { get; set; }
+
+        [Option("branch", Required = false, Default = "stable", HelpText = "Discord branch: `stable`, `canary`, `ptb`. Default: stable.")]
+        public string Branch { get; set; } = "stable";
+
+        [Option("host", Required = false, HelpText = "Proxy address.")]
+        public string Host { get; set; }
+
+        [Option("port", Required = false, HelpText = "Proxy port.")]
+        public int? Port { get; set; }
+
+        [Option("user", Required = false, HelpText = "Proxy user.")]
+        public string User { get; set; }
+
+        [Option("password", Required = false, HelpText = "Proxy password.")]
+        public string Password { get; set; }
+    }
+}
