9448af34514501ecb7629f61a2aa39eb82110ac9
snowluwu <the.snowluwu@gmail.com>
2026-07-12 18:52:06 +0600

    feat(installer): add mutex in program to check single instance

--- installer/Program.cs
+++ installer/Program.cs
@@ -1,14 +1,25 @@
 ﻿using Droute.Installer.Forms;
 using System;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Droute.Installer
 {
     internal static class Program
     {
+        private static bool createdNew;
+        private static Mutex mtx;
+
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            mtx = new Mutex(true, "snowluwu.droute", out createdNew);
+
+            // droute is already running
+            if (!createdNew)
+                return;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMain());
